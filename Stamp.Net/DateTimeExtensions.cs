3b9f6137c2e7531f38a42997647ea741471a5b5c
Slavo Ingilizov <slavo.ing@gmail.com>
2011-07-07 19:44:47 +0300

    Worked around the DateTime problem of using a custom formatter

--- Stamp.Net/DateTimeExtensions.cs
+++ Stamp.Net/DateTimeExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 
 namespace Stamp.Net
 {
@@ -18,7 +19,26 @@ namespace Stamp.Net
         /// <returns>A string representation of the <see cref="DateTime"/> object</returns>
         public static string Format(this DateTime dateTime, string example)
         {
-            return dateTime.ToString(example, new DateTimeExampleFormatter());
+            return dateTime.ToStringExtended("June 24, 2033", new DateTimeExampleFormatter());
+        }
+
+        /// <summary>
+        /// An alternative for the ToString() method of DateTime to work around a problem with implementing custom formatters for DateTime objects
+        /// The default implementation, even if given a good IFormatProvider implementation, would never call a custom formatter. It would
+        /// still use the default formatter.
+        /// </summary>
+        /// <param name="dateTime">The date time object.</param>
+        /// <param name="provider">The provider.</param>
+        /// <param name="format">The format.</param>
+        /// <returns></returns>
+        public static string ToStringExtended(this DateTime dateTime, string format, IFormatProvider provider)
+        {
+            var formatter = provider.GetFormat(typeof(DateTimeFormatInfo)) as ICustomFormatter;
+            if (formatter != null)
+            {
+                formatter.Format(format, dateTime, provider);
+            }
+            throw new ArgumentException("The format provider argument did not return an instance that implements ICustomFormatter", "provider");
         }
     }
 }
