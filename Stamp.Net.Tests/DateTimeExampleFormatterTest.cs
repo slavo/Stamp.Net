355c9216ca147fe7f6d6d56086e97a9af7350e5d
Slavo Ingilizov <slavo.ing@gmail.com>
2013-11-15 17:59:40 +0200

    include NUnit as a library and init unit tests

---
+++ Stamp.Net.Tests/DateTimeExampleFormatterTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Stamp.Net.Tests
+{
+    [TestFixture]
+    public class DateTimeExampleFormatterTest
+    {
+        [Test]
+        public void GetFormat_WithDateTimeFormatInfo_ReturnsObject()
+        {
+            var formatter = new DateTimeExampleFormatter();
+            var format = formatter.GetFormat(typeof(DateTimeFormatInfo));
+
+            Assert.AreEqual(format, formatter);
+        }
+
+        [Test]
+        public void GetFormat_WithRandomType_ReturnsNull()
+        {
+            var formatter = new DateTimeExampleFormatter();
+            var format = formatter.GetFormat(typeof(string));
+
+            Assert.IsNull(format);
+        }
+    }
+}
